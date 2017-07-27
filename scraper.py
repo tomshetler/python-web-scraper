e0737fb1bc048f71e286682c9f7f6df3845e2f8a
ThomasShetler <tshetler93@gmail.com>
2017-07-27 14:10:40 -0400

    Scrapes r/showerthoughts and writes all the thoughts to a text file

--- scraper.py
+++ scraper.py
@@ -1,6 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+def get_shower_thoughts():
+    page = requests.get("https://www.reddit.com/r/Showerthoughts/")
+    if page.status_code != 200:
+        get_shower_thoughts()
+    else:
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        shower_thoughts = list(soup.find_all('a', class_="title"))
+        for thought in shower_thoughts:
+            print(thought.get_text() + "\n")
+
+        try:
+            with open('shower_thoughts.txt', 'a+') as f:
+                for thought in shower_thoughts:
+                    f.write(str(thought.get_text().encode('utf8')))
+                    f.write("\n")
+        except FileNotFoundError:
+            with open('shower_thoughts.txt', 'w') as f:
+                for thought in shower_thoughts:
+                    f.write(str(thought.get_text().encode('utf8')))
+                    f.write("\n")
+
+get_shower_thoughts()
+
